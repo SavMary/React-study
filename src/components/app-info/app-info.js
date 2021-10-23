3ce0a135dd005d0bedf493191c428e0dc716b7ba
Maryna <savna023@gmail.com>
2021-10-23 17:25:16 +0300

    add employees project

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+import './app-info.css';
+const AppInfo = ({increased, employees}) => {
+    return (
+        <div className="app-info">
+            <h1>Облік працівників в компанії</h1>
+            <h2>Загальна кількість працівників:{employees} </h2>
+            <h2>Премію отримають: {increased}</h2>
+        </div>
+    )
+}
+export default AppInfo;
