533f53c79bd0a8ecb8706bfe422b4746f801d6d9
Maryna <savna023@gmail.com>
2022-06-25 20:34:42 +0300

    some changes

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,8 @@
-// import logo from './logo.png';
 import './app-info.css';
 const AppInfo = ({ increased, employees }) => {
-    // const logo = require('./logo.png');
     return (
         <div className="app-info">
             <h1>Облік працівників в компанії Pied Piper</h1>
-            {/* <img className="logo" src={require('./img/pp.png')} alt="logo"/> */}
             <h2>Загальна кількість працівників: {employees} </h2>
             <h2>Премію отримають: {increased}</h2>
         </div>
